c0befda7f7f453ff66eb23ba4900ec27ed5bd5cb
LexiXT <2696372135@qq.com>
2018-11-26 17:33:14 +0800

    add file

---
+++ plan/codes/leetcode/26_removeDuplicates.py
@@ -0,0 +1,32 @@
+ #!/usr/local/bin/python3
+ # -*- coding: UTF-8 -*-
+ 
+ # Filename: removeDuplicates.py
+ # author by: Lexi
+ 
+ class Solution:
+     def removeDuplicates(self, nums):
+         """
+         :type nums: List[int]
+         :rtype: int
+         """
+         #for i in range(len(nums)-1):
+         #    for j in nums[i+1:]:
+         #        if i == j:
+         #            nums.remove(nums[i])
+         #        else:
+         #            break
+         #print(nums)
+ 
+         i = 0
+         while i < len(nums)-1:
+             if nums[i] == nums[i+1]:
+                 nums.remove(nums[i])
+             else:
+                 i=i+1
+         return len(nums)
+ 
+ if __name__ == '__main__':
+     s = Solution()
+     res = s.removeDuplicates([0,1,1,2,2,3,3,4,5,5,5,5])
+     print(res)
