7b898ef8e6bdae86027d750dd9571c602f6e8719
LexiXT <2696372135@qq.com>
2019-05-12 22:52:13 +0800

    add leetcode

---
+++ plan/codes/leetcode2/1.py
@@ -0,0 +1,35 @@
+# -*- coding: UTF-8 -*-
+
+# Filename: sumTwo.py
+# author by: Lexi
+
+# 求两数之和
+
+class Solution:
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        # 方法一
+        # 暴力破解法
+        # for i in range(len(nums)):
+        #    for j in range(len(nums)):
+        #        if nums[i] + nums[j] == target:
+        #            return (i,j)
+
+        dict = {}
+        for i in range(len(nums)):
+            x = nums[i]
+            if target - x in dict:
+                return (dict[target - nums[i]], i)
+
+            dict[x] = i
+
+
+if __name__ == '__main__':
+    s = Solution()
+    result = s.twoSum([1, 5, 2, 7], 9)
+    print(result)
+
